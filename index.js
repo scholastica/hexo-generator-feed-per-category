52aad4e264a877e245f1e7d97ba277accaaf0e1c
Willem Meints <willemm@infosupport.com>
2016-04-12 16:15:13 +0200

    Add defaults for configuration

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var assign = require('object-assign');
 
 // Use the configuration below to provide defaults.
 // The user can override the defaults from the _config.yaml file.
-hexo.config.category_feed_generator = assign({
-
-}, hexo.config.category_feed_generator);
+hexo.config.feed = assign({
+    format: 'rss2',
+    limit: 20
+}, hexo.config.feed);
 
 // Register the generator with hexo.
 hexo.extend.generator.register('category-feed', require('./lib/generator'));
